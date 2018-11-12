9407ab96ceea27a69ac37318104a55155d7b93c3
hammad <hammadmehmood@gmail.com>
2018-11-12 11:02:04 +0500

    firebase added

--- app/src/main/java/com/hm/groupchat/MainActivity.java
+++ app/src/main/java/com/hm/groupchat/MainActivity.java
@@ -1,13 +1,34 @@
 package com.hm.groupchat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if(mAuth.getCurrentUser() == null) {
+
+            //Not logged in
+        }
+
+        else {
+
+
+        }
     }
 }
