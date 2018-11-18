94c3cddf8efc05e3ab66668412f42d5212134c57
hammad <hammadmehmood@gmail.com>
2018-11-18 12:39:05 +0500

    Messages UI started

--- app/src/main/java/com/hm/groupchat/Fragments/ChatFragment.java
+++ app/src/main/java/com/hm/groupchat/Fragments/ChatFragment.java
@@ -2,12 +2,16 @@ package com.hm.groupchat.Fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.hm.groupchat.R;
+import com.squareup.picasso.Picasso;
+import com.stfalcon.chatkit.commons.ImageLoader;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +28,21 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        ImageLoader imageLoader = new ImageLoader() {
+            @Override
+            public void loadImage(ImageView imageView, @Nullable String url, @Nullable Object payload) {
+
+                Picasso.get().load(url).into(imageView);
+            }
+        };
+
+        /*
+        MessagesListAdapter<Message> adapter = new MessagesListAdapter<>(senderId, imageLoader);
+        messagesList.setAdapter(adapter);*/
+
+        return view;
     }
 
 }
